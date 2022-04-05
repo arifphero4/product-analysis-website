8b771371b335877c8fd53642ebc6b28c48091173
arifphero <ahammadarif232@gmail.com>
2022-04-05 13:04:09 +0600

    footer edited

--- src/hooks/Footer.js
+++ src/hooks/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='py-8 text-center bg-gray-900 text-white text-lg'>
-       <div>Created by Arif Hasan &copy; 2022</div>
+    <div className="py-8 text-center bg-gray-900 text-white text-lg">
+      <div>Created by Arif Ahammad &copy; 2022</div>
     </div>
   );
 };
