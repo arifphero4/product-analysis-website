5e228b66dc73e9c82af9f20b2ff39c252c4fd711
arifphero <ahammadarif232@gmail.com>
2022-04-05 15:25:54 +0600

    _redirects file created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About';
-import Blogs from './Pages/Blogs';
-import Dashboard from './Pages/Dashboard';
-import Home from './Pages/Home';
-import Reviews from './Pages/Reviews';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About";
+import Blogs from "./Pages/Blogs";
+import Dashboard from "./Pages/Dashboard";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import Reviews from "./Pages/Reviews";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/review' element={<Reviews />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/blogs' element={<Blogs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/review" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
